a4bc5de268ac7708ad5b478217e9c07250886605
SolarSpectre <josephcaza32@gmail.com>
2024-11-20 20:34:41 -0500

    basic UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link";
 
 export default function HomePage() {
+  const mockUrls = [
+    "https://utfs.io/f/MLiSC9m1s4rCyInrtd6WBQ87rmVTOFqiUZe0PdpNIK4E5JLM",
+"https://utfs.io/f/MLiSC9m1s4rCMQD2Onm1s4rCy3jXGOf2qgT0ZRJnidQwb8KL",
+"https://utfs.io/f/MLiSC9m1s4rC9AMxkMHVaU5WkFTNu03CmypLx4cOM1DEzP8K",
+"https://utfs.io/f/MLiSC9m1s4rCco46v3ixlmhG8UnHT0zide3kwxgEIsyj1bLO",
+  ]
+  const mockImages = mockUrls.map((url,index)=>({
+    id: index+1,
+    url
+  }))
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress) 
+    <main className="">
+      <div className="flex flex-wrap gap-4">{
+        [...mockImages,...mockImages,...mockImages].map((image)=>(
+        <div key={image.id} className="w-48">
+          <img src={image.url}/>
+        </div>
+        ))
+      }</div>
     </main>
   );
 }
