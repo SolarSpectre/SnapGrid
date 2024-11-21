6f664237b15acb4fc502f7059a9e4ca2ab64b236
SolarSpectre <josephcaza32@gmail.com>
2024-11-20 21:14:26 -0500

    database integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
-
-export default function HomePage() {
+import db from "../server/db/index.ts";
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
   const mockUrls = [
     "https://utfs.io/f/MLiSC9m1s4rCyInrtd6WBQ87rmVTOFqiUZe0PdpNIK4E5JLM",
 "https://utfs.io/f/MLiSC9m1s4rCMQD2Onm1s4rCy3jXGOf2qgT0ZRJnidQwb8KL",
@@ -13,9 +14,13 @@ export default function HomePage() {
   }))
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">{
-        [...mockImages,...mockImages,...mockImages].map((image)=>(
-        <div key={image.id} className="w-48">
+      <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+        <div key={post.id}>{post.name}</div>
+        ))}
+        {
+        [...mockImages,...mockImages,...mockImages].map((image, index)=>(
+        <div key={image.id + "-" + index} className="w-48">
           <img src={image.url}/>
         </div>
         ))
