af1f83f1e7b6b9d3c971d9170f0474f51c049d72
SolarSpectre <josephcaza32@gmail.com>
2024-11-21 23:27:01 -0500

    add {}

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import db from "../server/db/index.ts";
 export const dynamic = "force-dynamic";
 export default async function HomePage() {
-  const images = await db.query.images.findMany(
+  const images = await db.query.images.findMany({
     orderBy: (model, {desc}) => desc(model.id)
-  );
+  });
     return (
     <main className="">
       <div className="flex flex-wrap gap-4">
