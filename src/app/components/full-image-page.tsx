3c06a8522da8ad4fe873c323d2555e675af52e21
SolarSpectre <josephcaza32@gmail.com>
2024-11-28 18:00:30 -0500

    changes on modal

--- src/app/components/full-image-page.tsx
+++ src/app/components/full-image-page.tsx
@@ -1,15 +1,19 @@
-import Image from "next/image";
 import { getImage } from "~/server/queries";
 
-export default async function FullPageImage(props:{id:number}) {
+export default async function FullPageImage(props: { id: number }) {
   const image = await getImage(props.id);
   return (
-      <Image
-        src={image.url}
-        style={{ objectFit: "contain" }}
-        width={384}
-        height={384}
-        alt={image.name}
-      />
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex-shrink flex items-center justify-center">
+        <img
+          src={image.url}
+          className="flex-shrink object-contain"
+          alt={image.name}
+        />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
   );
 }
