caae5651ee5f50589c74285ddf8a2b0fca5702d5
SolarSpectre <josephcaza32@gmail.com>
2024-11-30 11:17:15 -0500

    shadcn setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en" className={`${GeistSans.variable}`}>
+      <html lang="en" className={`${GeistSans.variable} font-sans dark`}>
         <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
         <body>
           <div className="grid h-screen grid-rows-[auto,1fr]">
