c2aba4ddd3b279f5e4755b683c46b099f37e6e3c
SolarSpectre <josephcaza32@gmail.com>
2024-11-30 11:51:28 -0500

    add loading spinner and styles for uploading state

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useUploadThing } from "~/utils/uploadthing";
+import { toast } from "sonner";
 
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
@@ -48,16 +49,49 @@ const UploadSVG = () => {
   );
 };
 
+
+function LoadingSpinnerSVG() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="white"
+    >
+      <path
+        d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
+        className="spinner_P7sC"
+      />
+    </svg>
+  );
+}
 export function SimpleUploadButton() {
   const router = useRouter();
   const { inputProps } = useUploadThingInputProps("imageUploader", {
+    onUploadBegin() {
+      toast(
+        <div className="flex items-center gap-2 text-white">
+          <LoadingSpinnerSVG />
+          <span className="text-lg">Uploading...</span>
+        </div>,
+        {
+          duration: 100000,
+          id: "upload-begin",
+        },
+      );
+    },
     onClientUploadComplete() {
+      toast.dismiss("upload-begin");
+      toast(<span className="text-lg">Upload Complete!</span>);
       router.refresh();
     },
   });
   return (
     <div>
-      <label htmlFor="upload-button" className="cursor-pointer"><UploadSVG /></label>
+      <label htmlFor="upload-button" className="cursor-pointer">
+        <UploadSVG />
+      </label>
       <input
         id="upload-button"
         type="file"
