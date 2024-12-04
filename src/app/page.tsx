ec57a85d07d296ac6d47407fc2fae32e76f068b4
SolarSpectre <josephcaza32@gmail.com>
2024-12-04 10:22:44 -0500

    Fix the page layout for images of different resolutions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,15 @@ const Images = async () => {
   return (
     <div className="flex flex-wrap justify-center gap-4 p-4">
       {images.map((image) => (
-        <div key={image.id} className="flex h-48 w-48 flex-col">
+        <div key={image.id} className="flex h-60 w-48 flex-col">
           <Link href={`/img/${image.id}`}>
           <Image
               src={image.url}
-              style={{ objectFit: "contain" }}
+              style={{ objectFit: "cover" }}
               width={192}
               height={192}
               alt={image.name}
+              className="aspect-square"
             />
           </Link>
           <div>{image.name}</div>
